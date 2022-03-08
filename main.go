a7e7b9548c73a6b55e0013255b884411ee560252
贺照民 <hezm@yiche.com>
2022-03-08 18:28:00 +0800

    add main

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	v1 "github.com/hezhaomin/raidutil/api/v1"
+	"github.com/hezhaomin/raidutil/providers"
+)
+
+func main() {
+	rt := v1.RaidType{
+		Type:    "stor",
+		BinPath: "/opt/MegaRAID/storcli/storcli64",
+	}
+	provider, err := providers.NewRaidProvider(&rt)
+	if err != nil {
+		os.Exit(1)
+	}
+	err = provider.Get()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(rt)
+}
